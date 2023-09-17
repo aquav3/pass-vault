29fd00668ecaa7e2664612a7bd26b723a255095c
aquav3 <kyrbynyt@gmail.com>
2023-09-17 22:31:50 +0300

    Added usage gtg to sleep rn

--- main.go
+++ main.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
+
+func usage() {
+    fmt.Println("./pass-vault [ARGS]")
+    fmt.Println("ARGS: ")
+    fmt.Println("-f [filename]")
+    fmt.Println("-a [add, remove, list]")
+    fmt.Println("----------------------")
+    fmt.Println("if add: ");
+    fmt.Println("    -u [username]")
+    fmt.Println("    -p [password]")
+    fmt.Println("endif add")
+    fmt.Println("----------------------")
+    fmt.Println("if remove: ")
+    fmt.Println("    -u [username to remove]")
+    fmt.Println("----------------------")
+}
 
 func main() {
-    fmt.Println("Hello world!");
+    fmt.Println("pass_vault")
+    if len(os.Args) < 5 {
+        usage()
+    }
+
 }
