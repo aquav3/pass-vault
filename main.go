5b6ed76cfaa8a5a65742318b0f690f1242770b9c
aquav3 <kyrbynyt@gmail.com>
2023-09-19 19:05:26 +0300

    Full functionality *Except no encryption yippie

--- main.go
+++ main.go
@@ -1,13 +1,15 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"log"
 	"os"
 )
 
 type User struct {
-    username string;
-    password string;
+    Username string;
+    Password string;
 }
 
 func usage() {
@@ -26,6 +28,35 @@ func usage() {
     fmt.Println("----------------------")
 }
 
+func ListUsers(users []User) {
+    fmt.Println("All users: ")
+    for _, user := range users {
+        fmt.Printf("Username: %s, Password: %s\n", user.Username, user.Password)
+    }
+}
+
+func AddUser(users *[]User, user User) {
+    *users = append(*users, user)
+}
+
+func RemoveUser(users *[]User, user User) {
+    for idx, usr := range *users {
+        if usr.Username == user.Username {
+            firstHalf := (*users)[:idx]
+            secondHalf := (*users)[idx+1:]
+            *users = append(firstHalf, secondHalf...)
+        } 
+    }
+}
+
+func Quit(outFileName string, users []User) {
+    data, _ := json.Marshal(users) 
+    file, _ := os.OpenFile(outFileName, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0644)
+    defer file.Close()
+    file.WriteString(string(data))
+
+}
+
 func main() {
     fmt.Println("pass_vault")
     if len(os.Args) < 5 {
@@ -33,20 +64,29 @@ func main() {
         os.Exit(1)
     }
 
+    outFile := os.Args[2]
     action := os.Args[4] 
+    
 
+    var users []User
+    filedata, _ := os.ReadFile(outFile)
+    err := json.Unmarshal(filedata, &users)
+    
+    if err != nil {
+        log.Fatalln(err)
+    }
     if action == "list" {
-        fmt.Println("well priting all the pass_vault")
+        ListUsers(users)
     }  
     if action == "add" {
         user := User {os.Args[6], os.Args[8]}
-        fmt.Println(user)    
+        AddUser(&users, user)
+        Quit(outFile, users)
     }
     if action == "remove" {
-        user_to_remove := User {"", os.Args[6]}
-        fmt.Println(user_to_remove)
+        userToRemove := User {os.Args[5], ""}
+        RemoveUser(&users, userToRemove)
+        fmt.Println(users) 
+        Quit(outFile, users)
     }
-
-    
-
 }
